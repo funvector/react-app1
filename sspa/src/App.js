1982e031c618360da8f31fc018c9623842763aab
Vlad K <huza007@mail.com>
2019-11-01 12:51:35 +0300

    feat: add some components

--- sspa/src/App.js
+++ sspa/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
 
-function App() {
-  return (
-    <>
-      
-    </>
-  );
+export default class App extends Component {
+  
+  render() {
+    return (
+      <Layout>
+        <Quiz />
+      </Layout>
+    );
+  }
 }
-
-export default App;
