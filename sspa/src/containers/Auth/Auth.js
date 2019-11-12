ad9e04fa9a1a412f0af1e4663d729706282e8731
Vlad K <huza007@mail.com>
2019-11-12 11:06:07 +0300

    feat: add some code

--- sspa/src/containers/Auth/Auth.js
+++ sspa/src/containers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
@@ -7,39 +7,39 @@ import is from 'is_js'
 export default class Auth extends Component {
 
   state = {
-      isFormValid: false,
-      formControls: {
-        email: {
-          value: '',
-          type: 'email',
-          label: 'Email',
-          errorMessage: 'Введите корректный email',
-          valid: false,
-          touched: false,
-          validation: {
-            required: true,
-            email: true
-          }
-        },
-        password: {
-          value: '',
-          type: 'password',
-          label: 'Пароль',
-          errorMessage: 'Введите корректный пароль',
-          valid: false,
-          touched: false,
-          validation: {
-            required: true,
-            minLength: 6
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
         }
       }
     }
   }
 
   loginHandler = () => {
-    
+
   }
-  
+
   registerHandler = () => {
 
   }
@@ -49,21 +49,21 @@ export default class Auth extends Component {
   }
 
   validateControl(value, validation) {
-    if(!validation) {
+    if (!validation) {
       return true
     }
 
     let isValid = true
 
-    if(validation.required) {
+    if (validation.required) {
       isValid = value.trim() !== '' && isValid
     }
 
-    if(validation.email) {
+    if (validation.email) {
       isValid = is.email(value) && isValid
     }
 
-    if(validation.minLength) {
+    if (validation.minLength) {
       isValid = value.length >= validation.minLength && isValid
     }
 
@@ -71,8 +71,8 @@ export default class Auth extends Component {
   }
 
   onChangeHandler = (event, controlName) => {
-    const formControls = {...this.state.formControls}
-    const control = {...formControls[controlName]}
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
 
     control.value = event.target.value
     control.touched = true
@@ -82,10 +82,10 @@ export default class Auth extends Component {
 
     let isFormValid = true
 
-    Object.keys(formControls).forEach((name) => {
+    Object.keys(formControls).forEach(name => {
       isFormValid = formControls[name].valid && isFormValid
     })
-    
+
     this.setState({
       formControls, isFormValid
     })
@@ -95,7 +95,7 @@ export default class Auth extends Component {
     return Object.keys(this.state.formControls).map((controlName, index) => {
       const control = this.state.formControls[controlName]
       return (
-        <Input 
+        <Input
           key={controlName + index}
           type={control.type}
           value={control.value}
@@ -104,7 +104,7 @@ export default class Auth extends Component {
           label={control.label}
           shouldValidate={!!control.validation}
           errorMessage={control.errorMessage}
-          onChange={(event) => this.onChangeHandler(event, controlName)}
+          onChange={event => this.onChangeHandler(event, controlName)}
         />
       )
     })
@@ -117,23 +117,23 @@ export default class Auth extends Component {
           <h1>Авторизация</h1>
 
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            
-            {this.renderInputs()}
 
-            <Button 
-                type='success' 
-                onClick={this.loginHandler}
-                disabled={!this.state.isFormValid}
-              >
-                Войти
+            { this.renderInputs() }
+
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Войти
             </Button>
 
-            <Button 
-                type='primary' 
-                onClick={this.registerHandler}
-                disabled={!this.state.isFormValid}
-              >
-                Зарегистрироваться
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Зарегистрироваться
             </Button>
           </form>
         </div>
--- sspa/src/containers/QuizCreator/QuizCreator.js
+++ sspa/src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
-import AuxIliary from '../../hoc/AuxIliary/AuxIliary'
 import Select from '../../components/UI/Select/Select'
-import { createControl } from '../../form/formFramework'
+import {createControl, validate, validateForm} from '../../form/formFramework'
+import Auxiliary from '../../hoc/Auxiliary/AuxIliary'
 
 function createOptionControl(number) {
   return createControl({
@@ -23,7 +23,7 @@ function createFormControls() {
     option1: createOptionControl(1),
     option2: createOptionControl(2),
     option3: createOptionControl(3),
-    option4: createOptionControl(4),
+    option4: createOptionControl(4)
   }
 }
 
@@ -31,43 +31,56 @@ export default class QuizCreator extends Component {
 
   state = {
     quiz: [],
+    isFormValid: false,
     rightAnswerId: 1,
     formControls: createFormControls()
   }
 
-  submitHandler = event => {
+  sibmitHandler = event => {
     event.preventDefault()
   }
 
-  addQuestionHandler = () => {
-
+  addQuestionHandler = event => {
+    event.preventDefault()
   }
-  
+
   createQuizHandler = () => {
 
   }
 
-  changeHandler = () => {
+  changeHandler = (value, controlName) => {
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
 
+    control.touched = true
+    control.value = value
+    control.valid = validate(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls,
+      isFormValid: validateForm(formControls)
+    })
   }
 
   renderControls() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
-      const control = this.state.formControls[controlName];
-  
+      const control = this.state.formControls[controlName]
+
       return (
-        <AuxIliary key={controlName + index}>
-          <Input 
+        <Auxiliary key={controlName + index}>
+          <Input
             label={control.label}
             value={control.value}
             valid={control.valid}
             shouldValidate={!!control.validation}
             touched={control.touched}
             errorMessage={control.errorMessage}
-            onChange={(event) => this.changeHandler(event.target.value, controlName)}
+            onChange={event => this.changeHandler(event.target.value, controlName)}
           />
-          {index === 0 ? <hr /> : null}
-        </AuxIliary>
+          { index === 0 ? <hr /> : null }
+        </Auxiliary>
       )
     })
   }
@@ -80,14 +93,14 @@ export default class QuizCreator extends Component {
 
   render() {
     const select = <Select
-      label='Выберите правильный ответ'
+      label="Выберите правильный ответ"
       value={this.state.rightAnswerId}
       onChange={this.selectChangeHandler}
       options={[
         {text: 1, value: 1},
         {text: 2, value: 2},
         {text: 3, value: 3},
-        {text: 4, value: 4},
+        {text: 4, value: 4}
       ]}
     />
 
@@ -96,27 +109,27 @@ export default class QuizCreator extends Component {
         <div>
           <h1>Создание теста</h1>
 
-          <form
-            onSubmit={this.submitHandler}
-          >
+          <form onSubmit={this.submitHandler}>
 
-          {this.renderControls()}
+            { this.renderControls() }
 
-          {select}
-          
-          <Button
-            type='primary'
-            onClick={this.addQuestionHandler}
-          >
-            Добавить вопрос
-          </Button>
+            { select }
 
-          <Button
-            type='success'
-            onClick={this.createQuizHandler}
-          >
-            Создать тест
-          </Button>
+            <Button
+              type="primary"
+              onClick={this.addQuestionHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Добавить вопрос
+            </Button>
+
+            <Button
+              type="success"
+              onClick={this.createQuizHandler}
+              disabled={this.state.quiz.length === 0}
+            >
+              Создать тест
+            </Button>
 
           </form>
         </div>
